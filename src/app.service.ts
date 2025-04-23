265dc6239fb47436653e3b865fadabbcd6c5665c
VuongTranDev <vuongtran975@gmail.com>
2025-04-23 11:05:36 +0700

    test update deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'test Hello World!';
+    return 'Hello render.com!';
   }
 }
